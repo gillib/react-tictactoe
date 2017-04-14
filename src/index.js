eb2929673510cc3787cb2b3b863920e2b23a4a70
gilbartsion <gillib@gmail.com>
2017-04-14 18:03:49 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './components/game/game';
+import './components/components.css';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('container')
+);
